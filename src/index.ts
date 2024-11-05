7460b5b410d416367d0d66c065da38d1589aab4f
Azat Azamat <azatkariuly@Azats-MacBook-Pro.local>
2024-11-05 16:06:09 +0900

    allow cors

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import productsRoute from './routes/products.route.js'
 const port = 8000
 
 const app = express()
-const allowedOrigins = ['*', 'http://localhost:3000'];
+const allowedOrigins = ['*', 'http://localhost:3001'];
 const corsConfig = {
     origin: allowedOrigins,
     credentials: true,
